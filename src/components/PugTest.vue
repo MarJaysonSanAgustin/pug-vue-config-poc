aafb2efdac1fbbd7b498381aa470fadb30286843
Mar Jayson San Agustin <sanagustinjayson@gmail.com>
2020-06-02 16:56:39 +0800

    feat: add pug config

---
+++ src/components/PugTest.vue
@@ -0,0 +1,17 @@
+<template lang="pug">
+  div
+    .awesome-class
+      h1 Hello World - Pug!
+    p Outside
+
+</template>
+
+<style lang="scss">
+.awesome-class {
+  font-size: 5em;
+  
+  h1 {
+    color: aqua;
+  }
+}
+</style>
